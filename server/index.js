3ec98593d01f6b9163f642304c81516930e6c8c0
JefreyTG <yerffej01@gmail.com>
2023-11-13 13:41:05 -0500

    problema solucionado front comunicandose con back end

--- server/index.js
+++ server/index.js
@@ -1,31 +1,38 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const mysql = require("mysql");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "",
-    database:"inventario_crud",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "productos-inventario",
 });
 
-app.post("/create", (req, res)=>{
-    const product =req.body.product;
-    const amount = req.body.amount;
-    const price = req.body.price;
+app.post("/create", (req, res) => {
+  const product = req.body.product;
+  const amount = req.body.amount;
+  const price = req.body.price;
 
-    db.query("INSERT INTO productos inventario(producto,amount,price) VALUE (?,?,?)", [product,amount,price],
-    
-    (err, result)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.send("Producto añadido con exito!")
-        }
+  db.query(
+    "INSERT INTO productos_inventario(product,amount,price) VALUES (?,?,?)",
+    [product, amount, price],
+
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al añador el producto");
+      } else {
+        res.send("Producto añadido con exito!");
+      }
     }
-    ); 
+  );
 });
 
-app.listen(3001, ()=>{
-    console.log("Corriendo en el puerto 3001")
-})
+app.listen(3001, () => {
+  console.log("Corriendo en el puerto 3001");
+});
